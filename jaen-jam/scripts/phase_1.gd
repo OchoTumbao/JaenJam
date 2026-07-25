6f481aadaf332dffe3334e634315d43e87706dc7
marta☆°ミ <66879977+ComputerDreams@users.noreply.github.com>
2026-07-26 01:37:13 +0200

    Added phase 1 hitbox (#16)

--- jaen-jam/scripts/phase_1.gd
+++ jaen-jam/scripts/phase_1.gd
@@ -2,4 +2,16 @@ extends Node2D
 
 func _ready() -> void:
 	Global.spawn_bee(self, false, 1920)
-	Global.spawn_cat(self, true, 1000)
+	
+
+func _on_cat_wall_body_entered(body: Node2D) -> void:
+	Global.spawn_cat(self, true, 4000)
+
+
+func _on_taxi_wall_body_entered(body: Node2D) -> void:
+	Global.spawn_taxi(self, true, 6000)
+	
+
+
+func _on_cart_wall_body_entered(body: Node2D) -> void:
+		Global.spawn_cart(self, true, 7000)
