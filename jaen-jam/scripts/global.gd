cad1e1ec156105f522c18bf395f5ced5235a7771
Marta Sánchez Pavón <marta_sanchez_98@hotmail.es>
2026-07-26 00:01:40 +0200

    Added sprites and background

--- jaen-jam/scripts/global.gd
+++ jaen-jam/scripts/global.gd
@@ -44,9 +44,16 @@ func set_scores(entity: Node2D, angel_score: int, devil_score: int, ignore_score
 	
 
 func spawn_bee(parent: Node, jumpable: bool, horizontalPosition: int):
-	print("spawn_bee llamado")
 	var entity = preload("res://entity.tscn").instantiate()
-	print(entity)
+	entity.set_animation("bee")
 	place_entity(jumpable,horizontalPosition,entity)
 	set_scores(entity,-5,-10,10,-5)
 	parent.add_child(entity)
+	
+func spawn_cat(parent: Node, jumpable: bool, horizontalPosition: int):
+	var entity = preload("res://entity.tscn").instantiate()
+	entity.set_animation("cat")
+	place_entity(jumpable,horizontalPosition,entity)
+	set_scores(entity,-5,-10,10,-5)
+	parent.add_child(entity)
+	
