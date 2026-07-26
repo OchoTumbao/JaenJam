b524c9c24390d85581f443e1ecbcfec23cba8034
Moisés Téllez Francisco <moisestellezfrancisco@gmail.com>
2026-07-26 03:10:45 +0200

    Entities and camera fixes

--- jaen-jam/characters/player/player.gd
+++ jaen-jam/characters/player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var bullet_scene = preload("res://characters/player/bullet.tscn")
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -600.0
 
 
 func _physics_process(delta: float) -> void:
--- jaen-jam/scripts/global.gd
+++ jaen-jam/scripts/global.gd
@@ -1,14 +1,18 @@
 extends Node
 
 var gameController : GameController
-var entitySpeed = 10
 var score = 0
-const RAIL1 = 540
-const RAIL2 = 580
 
 enum Dialogue_mode {
 	MANUAL,
 	AUTO
+}
+
+enum Entity_Hitboxes {
+	BEE,
+	CART,
+	CAT,
+	TAXI
 }
 
 func loadDialogueJson(path: String) -> Array:
@@ -30,12 +34,6 @@ func loadDialogueJson(path: String) -> Array:
 func modifyScore(value: int):
 	score += value
 
-func place_entity(jumpable: bool, horizontalPosition: int, entity: Node2D):
-		if jumpable:
-			entity.set_initial_position(Vector2(horizontalPosition,RAIL2))
-		else:
-			entity.set_initial_position(Vector2(horizontalPosition,RAIL1))
-
 func set_scores(entity: Node2D, angel_score: int, devil_score: int, ignore_score: int, player_hit_score: int):
 	entity.set_angel_score(angel_score)
 	entity.set_devil_score(devil_score)
@@ -43,31 +41,39 @@ func set_scores(entity: Node2D, angel_score: int, devil_score: int, ignore_score
 	entity.set_player_hit_score(player_hit_score)
 	
 
-func spawn_bee(parent: Node, jumpable: bool, horizontalPosition: int):
+func spawn_bee(parent: Node, horizontalPosition: int, verticalPosition: int, speed: int):
 	var entity = load("res://entity.tscn").instantiate()
 	entity.set_animation("bee")
-	place_entity(jumpable,horizontalPosition,entity)
+	entity.set_speed(speed)
+	entity.set_hitbox(Global.Entity_Hitboxes.BEE)
+	entity.set_initial_position(Vector2(horizontalPosition,verticalPosition))
 	set_scores(entity,-5,-10,10,-5)
 	parent.add_child(entity)
 	
-func spawn_cat(parent: Node, jumpable: bool, horizontalPosition: int):
+func spawn_cat(parent: Node, horizontalPosition: int, verticalPosition: int, speed: int):
 	var entity = load("res://entity.tscn").instantiate()
 	entity.set_animation("cat")
-	place_entity(jumpable,horizontalPosition,entity)
-	set_scores(entity,-5,-10,10,-5)
+	entity.set_speed(speed)
+	entity.set_hitbox(Global.Entity_Hitboxes.CAT)
+	entity.set_initial_position(Vector2(horizontalPosition,verticalPosition))
+	set_scores(entity,5,-10,-5,10)
 	parent.add_child(entity)
 	
-func spawn_cart(parent: Node, jumpable: bool, horizontalPosition: int):
+func spawn_cart(parent: Node,horizontalPosition: int, verticalPosition: int, speed: int):
 	var entity = load("res://entity.tscn").instantiate()
 	entity.set_animation("carrito")
-	place_entity(jumpable,horizontalPosition,entity)
-	set_scores(entity,-5,-10,10,-5)
+	entity.set_speed(speed)
+	entity.set_hitbox(Global.Entity_Hitboxes.CART)
+	entity.set_initial_position(Vector2(horizontalPosition,verticalPosition))
+	set_scores(entity,10,-5,-10,5)
 	parent.add_child(entity)
 	
-func spawn_taxi(parent: Node, jumpable: bool, horizontalPosition: int):
+func spawn_taxi(parent: Node, horizontalPosition: int, verticalPosition:int, speed: int):
 	var entity = load("res://entity.tscn").instantiate()
 	entity.set_animation("taxi")
-	place_entity(jumpable,horizontalPosition,entity)
-	set_scores(entity,-5,-10,10,-5)
+	entity.set_speed(speed)
+	entity.set_hitbox(Global.Entity_Hitboxes.TAXI)
+	entity.set_initial_position(Vector2(horizontalPosition,verticalPosition))
+	set_scores(entity,-10,10,-5,-5)
 	parent.add_child(entity)
 	
